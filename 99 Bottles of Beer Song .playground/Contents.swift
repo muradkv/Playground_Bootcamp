047d91d819a5123911ea320d1773c3362f1c455a
murad <muradkv@gmail.com>
2019-03-10 16:30:52 +0300

    Use functions and loops

--- 99 Bottles of Beer Song .playground/Contents.swift
+++ 99 Bottles of Beer Song .playground/Contents.swift
@@ -2,9 +2,24 @@ import UIKit
 
 // 99 bottles of beer song first try
 
-var someArray = [Int](1...99)
- 
-for i in someArray.reversed() {
-    print("\(i) bottles of beer on the wall. \(i) of beer.")
-    print("Take one down and pass it around, \(i - 1) bottles of beer on the wall")
+//var someArray = [Int](1...99)
+//
+//for i in someArray.reversed() {
+//    print("\(i) bottles of beer on the wall. \(i) of beer.")
+//    print("Take one down and pass it around, \(i - 1) bottles of beer on the wall")
+//}
+
+// Second try ith functions and loops
+
+func beerSong() -> String {
+    var lyrics = ""
+    
+    for number in 1...99 {
+        let newLine = "\n\(100 - number) bottles of beer on the wall, \(100 - number) bottles of beer. \nTake one down and pass it around, \(100 - number - 1) bottles of beer on the wall.\n"
+        lyrics += newLine
+    }
+    
+    return lyrics
 }
+
+print(beerSong())
