242e57d5449e0b1bbe93e98cb0c633d053961836
murad <muradkv@gmail.com>
2019-03-10 16:39:53 +0300

    Use conditional statement for 1 bottle of beer

--- 99 Bottles of Beer Song .playground/Contents.swift
+++ 99 Bottles of Beer Song .playground/Contents.swift
@@ -9,15 +9,27 @@ import UIKit
 //    print("Take one down and pass it around, \(i - 1) bottles of beer on the wall")
 //}
 
-// Second try ith functions and loops
+// Second try with functions and loops
 
 func beerSong(howManyBeers: Int) -> String {
     var lyrics = ""
     
     for number in (1...howManyBeers).reversed() {
-        let newLine = "\n\(number) bottles of beer on the wall, \(number) bottles of beer. \nTake one down and pass it around, \(number - 1) bottles of beer on the wall.\n"
-        lyrics += newLine
+        
+        if number == 2 {
+            let newLine = "\n\(number) bottle of beer on the wall, \(number) bottle of beer. \nTake one down and pass it around, \(number - 1) bottle of beer on the wall.\n"
+            lyrics += newLine
+        } else if number == 1 {
+            let newLine = "\n\(number) bottle of beer on the wall, \(number) bottle of beer. \nTake one down and pass it around, no more bottles of beer on the wall\n"
+            lyrics += newLine
+        } else {
+            let newLine = "\n\(number) bottles of beer on the wall, \(number) bottles of beer. \nTake one down and pass it around, \(number - 1) bottles of beer on the wall.\n"
+            lyrics += newLine
+        }
+        
     }
+    
+    lyrics += "\nNo more bottles of beer on the wall, no more bottles of beer. \nGo to the store and buy some more, \(howManyBeers) bottles of beer on the wall."
     
     return lyrics
 }
