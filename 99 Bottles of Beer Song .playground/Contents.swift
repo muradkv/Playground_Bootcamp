d2b20dfaee7fdb1cbe0ced31dd556deb7be76602
murad <muradkv@gmail.com>
2019-03-10 16:25:21 +0300

    First Try
    Only loops, without functions

--- 99 Bottles of Beer Song .playground/Contents.swift
+++ 99 Bottles of Beer Song .playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
+// 99 bottles of beer song first try
 
+var someArray = [Int](1...99)
+ 
+for i in someArray.reversed() {
+    print("\(i) bottles of beer on the wall. \(i) of beer.")
+    print("Take one down and pass it around, \(i - 1) bottles of beer on the wall")
+}
